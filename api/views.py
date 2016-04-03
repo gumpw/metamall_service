18b1d4f5d3ae32a3288615518d9775479d90820c
gumpw <out_lier@126.com>
2016-04-03 15:23:57 +0800

    init

--- api/views.py
+++ api/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.decorators import api_view
+
+
+@api_view("POST")
+def login(request):
+    pass
+
+
+@api_view("POST")
+def register(request):
+    pass
+
+
+@api_view("POST")
+def province(request):
+    pass
+
+
+@api_view("POST")
+def city(request):
+    pass
+
+
+@api_view("POST")
+def area(request):
+    pass
+
+
+@api_view("POST")
+def area(request):
+    pass
+
+
+@api_view("POST")
+def sms(request):
+    pass
+
+
+@api_view("POST")
+def goods(request):
+    pass
